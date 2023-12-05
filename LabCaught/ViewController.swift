b361403052bdcbe2954cb0f39808d2a7ba7b1ae0
Shooq <Shooq.janahi17@hotmail.com>
2023-12-05 11:00:15 +0300

    Initial Commit

---
+++ LabCaught/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabCaught
+//
+//  Created by mobileProg on 05/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
