ac5e0cb4bfaaba7ce96be5d10b2bb7c48fe2cd72
Shooq <Shooq.janahi17@hotmail.com>
2023-12-12 08:16:21 +0300

    added properties to the user.swift

--- LabCaught/Models/Users.swift
+++ LabCaught/Models/Users.swift
@@ -6,12 +6,36 @@
 //
 
 import Foundation
-struct User {
-    var username: String
-    var passwordHash: String
+
+enum UserType {
+    case admin
+    case patient
+    case lab
+}
+
+
+struct User {  //defining a structure named User
+    var firstName:String
+    var lastName: String
+    var DOB: String //Date of birth
+    var CPR: Int
+    var PhoneNumber: Int
+    var username: String //declaring a property named username
+    var passwordHash: String //passwordHash is used to store hashed version of the users password for security
+    var confirmPassword: String
+    var userType: UserType
     
-    init(username: String, passwordHash: String) {
-        self.username = username
+    init(firstName:String, lastName:String, DOB:String, CPR: Int, PhoneNumber: Int, username: String, passwordHash: String, confirmPassword: String, userType:UserType) { //declaring an initializer for the User structure. inorder to call when a new instance of the structer is created
+        
+        self.firstName = firstName
+        self.lastName = lastName
+        self.DOB = DOB
+        self.CPR = CPR
+        self.PhoneNumber = PhoneNumber
+        self.username = username //this line assigns the value passed as the username parametter to the username propertyof the structure
+        
         self.passwordHash = passwordHash
+        self.confirmPassword = confirmPassword
+        self.userType = userType
     }
 }
