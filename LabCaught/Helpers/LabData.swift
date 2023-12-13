8f14bca04a18f2b6d51bda7d1d44b24ce6b4c4bf
Maryam Salah Hashem Adnan Saleh <202100106@BP-36-201-22.local>
2023-12-13 16:30:39 +0300

    o

--- LabCaught/Helpers/LabData.swift
+++ LabCaught/Helpers/LabData.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 class LabData {
-    static var labs = [Labs]()
+   // static var labs = [Labs]()
     
-    static func load(){
-        if labs.isEmpty {
-            labs = sampleLabs
-        }
-    }
+    //static func load(){
+      //  if labs.isEmpty {
+          //  labs = sampleLabs
+        //}
+   // }
     
-    static var sampleLabs = [
-        Labs(Username: "RoyalMedical", Password: "RM123", labName: "Royal Medical Services", Specialization: "Hospital", Location: "Riffa, Bahrain", ContactInfo: 17766666, alwaysOpen: true)
-    ]
+    //static var sampleLabs = [
+      //  Labs(Username: "RoyalMedical", Password: "RM123", labName: "Royal Medical Services", Specialization: "Hospital", Location: "Riffa, Bahrain", ContactInfo: 17766666, alwaysOpen: true)
+   // ]
 }
