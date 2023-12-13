0d9c5a4e72f91ded71046f6db008a11f1af9fcfe
Shaika7snn <202001432@student.potechnic.bh>
2023-12-13 12:05:46 +0300

    Admin

---
+++ LabCaught/Helpers/HospitalLabTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  HospitalLabTableViewCell.h
+//  LabCaught
+//
+//  Created by Shaikha Hasan Ali Hasan Ali Mohamed on 13/12/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HospitalLabTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LabCaught/Helpers/LabData.swift
@@ -0,0 +1,22 @@
+//
+//  LabData.swift
+//  LabCaught
+//
+//  Created by Shaikha Hasan Ali Hasan Ali Mohamed on 13/12/2023.
+//
+
+import Foundation
+
+class LabData {
+    static var labs = [Labs]()
+    
+    static func load(){
+        if labs.isEmpty {
+            labs = sampleLabs
+        }
+    }
+    
+    static var sampleLabs = [
+        Labs(Username: "RoyalMedical", Password: "RM123", labName: "Royal Medical Services", Specialization: "Hospital", Location: "Riffa, Bahrain", ContactInfo: 17766666, alwaysOpen: true)
+    ]
+}
