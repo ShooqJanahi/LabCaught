d7c9d8805f029c53df466f87c4e8427d52145815
Maryam Salah Hashem Adnan Saleh <202100106@BP-36-201-22.local>
2023-12-14 18:49:17 +0300

    m

--- LabCaught/Helpers/bookings.swift
+++ LabCaught/Helpers/bookings.swift
@@ -12,25 +12,16 @@ class AppData{
     
     static func load(){
         if bookings.isEmpty {
-            // bookings = sampleBookings
+             bookings = sampleBookings
         }
     }
     
-    /*static var sampleBookings = [booking(booking_date:
-     DateComponents(calendar: Calendar.current, year: 2023, month: 12, day:22), patient: Patient(firstName: "Maryam", lastName: "Salah", CPR: 123456789, DOB: DateComponents(calendar: Calendar.current, year: 2003, month: 11, day:22), user: User(firstName: "Maryam", lastName: "Salah", PhoneNumber: 12345678, username: "marym", password: "1234", confirmPassword: "1234", userType: .patient)), test: test(TestName: "Vitamin B12 Test", cost: "3 BHD", describtion: "Blood test for Vitamin B12", insrtuctions: "fasting for 8 to 10 hours is required")),
-     booking  (booking_date:
-     DateComponents(calendar: Calendar.current, year: 2024, month: 11, day:22), patient: Patient(firstName: "Fatima", lastName: "Salah", CPR: 123456785, DOB: DateComponents(calendar: Calendar.current, year: 2003, month: 11, day:22), user: User(firstName: "Fatima", lastName: "Salah", PhoneNumber: 12345678, username: "fatima", password: "1234", confirmPassword: "1234", userType: .patient)), test: test(TestName: "Vitamin B12 Test", cost: "3 BHD", describtion: "Blood test for Vitamin B12", insrtuctions: "fasting for 8 to 10 hours is required"))
-     
-     ]
-     }
-     
-     
-     
-     var book = booking(booking_date:
-     DateComponents(calendar: Calendar.current, year: 2023, month: 12, day:22), patient: Patient(firstName: "Maryam", lastName: "Salah", CPR: 123456789, DOB: DateComponents(calendar: Calendar.current, year: 2003, month: 11, day:22), user: User(firstName: "Maryam", lastName: "Salah", PhoneNumber: 12345678, username: "marym", password: "1234", confirmPassword: "1234", userType: .patient)), test: test(TestName: "Vitamin B12 Test", cost: "3 BHD", describtion: "Blood test for Vitamin B12", insrtuctions: "fasting for 8 to 10 hours is required"))
-     
-     var book1 = booking(booking_date:
-     DateComponents(calendar: Calendar.current, year: 2024, month: 11, day:22), patient: Patient(firstName: "Fatima", lastName: "Salah", CPR: 123456785, DOB: DateComponents(calendar: Calendar.current, year: 2003, month: 11, day:22), user: User(firstName: "Fatima", lastName: "Salah", PhoneNumber: 12345678, username: "fatima", password: "1234", confirmPassword: "1234", userType: .patient)), test: test(TestName: "Vitamin B12 Test", cost: "3 BHD", describtion: "Blood test for Vitamin B12", insrtuctions: "fasting for 8 to 10 hours is required"))
-     */
+    static var sampleBookings = [
+        booking(booking_date: DateComponents(calendar: Calendar.current, year: 2023, month: 12, day:22), patient: Patient(PhoneNumber: 39993999, username: "maryams", password: "1234", confirmPassword: "1234", userType: .patient, firstName: "Maryam", lastName: "Salah", DOB:  DateComponents(calendar: Calendar.current, year: 2003, month: 11, day:22), CPR: 0311001100), test: test(TestName: "Vitamin B12", cost: "3 BHD", describtion: "Blood test for Vitamin B12", insrtuctions: "fasting for 8 to 10 hours is required")),
+                                 
+        booking(booking_date: DateComponents(calendar: Calendar.current, year: 2023, month: 11, day:19), patient: Patient(PhoneNumber: 388838888, username: "Fatiman", password: "1234", confirmPassword: "1234", userType: .patient, firstName: "Fatima", lastName: "Ali", DOB:  DateComponents(calendar: Calendar.current, year: 2002, month: 09, day:02), CPR: 0209001100), test: test(TestName: "Vitamin D", cost: "3 BHD", describtion: "Blood test for Vitamin D", insrtuctions: "fasting for 8 to 10 hours is required"))
+        ]
+
+    
     
 }
