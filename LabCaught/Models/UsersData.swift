6a6256ccda2629f5c063270bda19d60f166b603a
Maryam Salah Hashem Adnan Saleh <202100106@BP-36-201-22.local>
2023-12-14 19:08:32 +0300

    m

--- LabCaught/Models/UsersData.swift
+++ LabCaught/Models/UsersData.swift
@@ -7,48 +7,9 @@
 
 import Foundation
 
-// Assuming this is a class to manage your users data
-// UsersData.swift
-import Foundation
-
-class UsersData {
-    var users: [User] = []
-
-    
-    //lab user
-    func addLabUser(PhoneNumber: Int, username: String, password: String, confirmPassword: String, Location: String, labName: String, alwaysOpen: Bool, openingTime: DateComponents, closingTime: DateComponents) {
-        
-        let newLabUser = Labs(PhoneNumber: PhoneNumber, username: username, password: password, confirmPassword: confirmPassword, userType: .lab, Location: Location, labName: labName, alwaysOpen: alwaysOpen, openingTime: openingTime, closingTime: closingTime)
-        
-        users.append(newLabUser)
-    }
-    // Additional methods and properties for managing users
-    
-    
-    
-   
-//admin user
-    func addAdminUser(PhoneNumber: Int, username: String, password: String, confirmPassword: String, department: String, firstName: String, lastName: String) {
-        
-        let newAdminUser = Admin(PhoneNumber: PhoneNumber, username: username, password: password, confirmPassword: confirmPassword, userType: .Admin, department: department, firstName: firstName, lastName: lastName)
-        
-        
-        // Append the new Admin user to the users array
-        users.append(newAdminUser)
-    }
-
-    
-    // ... Other methods
-}
 
 
-// This part should be called from within an appropriate method elsewhere in your app, such as AppDelegate or a ViewController:
-/*
-let usersData = UsersData()
-usersData.addLabUser(PhoneNumber: 1234567890, username: "labUser1", password: "password123", confirmPassword: "password123", Location: "123 Lab Street", labName: "Central Lab", alwaysOpen: false, openingTime: DateComponents(hour: 9, minute: 0), closingTime: DateComponents(hour: 17, minute: 0))
-*/
 
-
- 
-
+//creating a user instance for admin
+//let adminUser = User(firstName: "John", lastName: "Doe", PhoneNumber: 39874679, username: "admin", password: "admin123", confirmPassword: "admin123", userType: .Admin)
 
