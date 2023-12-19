89359bae073fa257f851e53e9808a9900c36d3a3
nadaalawadhi <nadaalawadhi4@gmail.com>
2023-12-19 20:14:35 +0300

    AppData

--- LabCaught/PatientHomeTableViewController.swift
+++ LabCaught/PatientHomeTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class PatientHomeTableViewController: UITableViewController {
     
     //IDK if this is right
-    var dataSource: [AnyObject] = LabData.labs + ServiceData.services
+    var dataSource: [AnyObject] = AppData.labs + AppData.services
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,41 +38,6 @@ class PatientHomeTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         <#code#>
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
