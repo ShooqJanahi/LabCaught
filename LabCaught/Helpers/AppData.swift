e2aa02e5ebef63a07ab3571a4796971ea04ebb35
Shooq <Shooq.janahi17@hotmail.com>
2023-12-20 13:10:46 +0300

    added lab user

--- LabCaught/Helpers/AppData.swift
+++ LabCaught/Helpers/AppData.swift
@@ -5,15 +5,37 @@
 //  Created by Nada Naser Ahmed Abdulla Abdulkarim Alawadhi on 19/12/2023.
 //
 
-import Foundation
+
 
 import Foundation
 
 struct AppData {
+    
+    
+    
+    
+    //Users
     static var users: [User] = []
+    
+    //Admin user
     static var admins: [Admin] = [
-        Admin(username: "admin", password: "admin123", name: "Admin User", department: "IT", firstName: "Alice", lastName: "Administrator", phoneNumber: 12345678)
+        Admin(username: "admin", password: "admin123", name: "Alice", department: "IT", firstName: "Alice", lastName: "Russo", phoneNumber: 12345678)
     ]
+    
+    
+    static var labs: [Labs] = [
+           Labs(
+               username: "labUser",
+               password: "labPassword",
+               phoneNumber: 12345678,
+               name: "Lab Name",
+               location: "Lab Location",
+               labName: "Lab 1",
+               alwaysOpen: true,
+               openingTime: DateComponents(hour: 9, minute: 0),
+               closingTime: DateComponents(hour: 17, minute: 0)
+           )
+       ]
     
     // Method to check if a username is already in use within the users array
     static func isUsernameInUse(username: String) -> Bool {
