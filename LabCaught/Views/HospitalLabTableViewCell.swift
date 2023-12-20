a06a68a484bd4acac5a4f7241678c75b4fd1dbd1
Shaikha7snn <202001432@student.polytechnic.bh>
2023-12-20 12:28:25 +0300

    Admin

---
+++ LabCaught/Views/HospitalLabTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HospitalLabTableViewCell.swift
+//  LabCaught
+//
+//  Created by Shaikha Hasan Ali Hasan Ali Mohamed on 20/12/2023.
+//
+
+import UIKit
+
+class HospitalLabTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
