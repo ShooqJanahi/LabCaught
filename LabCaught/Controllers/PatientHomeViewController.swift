c94891a9f4248835fb155dc81649c09c56c295e2
nadaalawadhi <nadaalawadhi4@gmail.com>
2023-12-20 19:06:17 +0300

    aa

---
+++ LabCaught/Controllers/PatientHomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PatientHomeViewController.swift
+//  LabCaught
+//
+//  Created by Nada Naser Ahmed Abdulla Abdulkarim Alawadhi on 20/12/2023.
+//
+
+import UIKit
+/*
+class PatientHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    /*
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return
+    }
+    */
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+*/
