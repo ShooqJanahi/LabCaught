0fb57885f878e6fd174b2d409c6149d7803ee5d5
nadaalawadhi <nadaalawadhi4@gmail.com>
2023-12-21 17:06:52 +0300

    cell

---
+++ PatientHomeFacilityTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PatientHomeFacilityTableViewCell.swift
+//  LabCaught
+//
+//  Created by Nada Naser Ahmed Abdulla Abdulkarim Alawadhi on 21/12/2023.
+//
+
+import UIKit
+
+class PatientHomeFacilityTableViewCell: UITableViewCell {
+    
+
+}
