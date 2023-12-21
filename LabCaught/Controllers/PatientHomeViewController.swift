10f58f86b967a3ecda7015660e96f15775feeff8
nadaalawadhi <nadaalawadhi4@gmail.com>
2023-12-21 17:19:29 +0300

    allTestsPackages array in appdata

--- LabCaught/Controllers/PatientHomeViewController.swift
+++ LabCaught/Controllers/PatientHomeViewController.swift
@@ -7,24 +7,19 @@
 
 import UIKit
 
-class PatientHomeViewController: UIViewController{
-    
-}
-
-
-/*
 class PatientHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
-    /*
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return
     }
-    */
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,4 +39,3 @@ class PatientHomeViewController: UIViewController, UITableViewDelegate, UITableV
     */
 
 }
-*/
