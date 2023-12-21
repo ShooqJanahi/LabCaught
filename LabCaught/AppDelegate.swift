31c8b471d10f22cd66d31c27801c45cbed6c3407
nadaalawadhi <nadaalawadhi4@gmail.com>
2023-12-21 19:19:23 +0300

    added the load function

--- LabCaught/AppDelegate.swift
+++ LabCaught/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        AppData.load()
         AppData.initializeAppData()
         // ... other setup code ...
         return true
