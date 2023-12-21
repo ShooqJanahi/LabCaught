91165c32fe6c3b831bc5d6859a7810863f03b978
Maryam Salah Hashem Adnan Saleh <202100106@BP-36-201-19.local>
2023-12-21 16:35:46 +0300

    segments

--- LabCaught/Controllers/patientBookingTableViewController.swift
+++ LabCaught/Controllers/patientBookingTableViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class patientBookingTableViewController: UITableViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    func categorizeBookings() {
+        upcomingBookings = listOfBookings.filter{
+            $0.status == .upcoming
+        }
+        completedBookings = listOfBookings.filter{
+            $0.status == .completed
+        }
+        cancelledBookings = listOfBookings.filter{
+            $0.status == .cancelled
+        }
+    }
+
+    
+    //declare
+    var listOfBookings = AppData.bookings
+    var upcomingBookings : [booking] = []
+    var completedBookings : [booking] = []
+    var cancelledBookings : [booking] = []
+    var selectedSegement = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       categorizeBookings()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -19,6 +41,10 @@ class patientBookingTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    @IBAction func segmentedControlChanged(_ sender: Any) {
+        selectedSegement = segmentedControl.selectedSegmentIndex
+        tableView.reloadData()
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -28,23 +54,41 @@ class patientBookingTableViewController: UITableViewController {
     var bookings = AppData.sampleBookings
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        bookings.count
+        switch selectedSegement {
+               case 0: return upcomingBookings.count
+               case 1: return completedBookings.count
+               case 2: return cancelledBookings.count
+               default: return bookings.count
+               }
+     
     }
-
 
  
  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "book", for: indexPath) as! PatientHistoryTableViewCell
-        let book = bookings[indexPath.row]
-        cell.testName.text = book.medicalService.name
-        cell.labName.text = "alo"
+        
+        let book: booking
+        switch selectedSegement {
+        case 0: book = upcomingBookings[indexPath.row]
+        case 1: book = completedBookings[indexPath.row]
+        case 2: book = cancelledBookings[indexPath.row]
+        default: fatalError("Invalid segment")
+        }
+
+    //    let book = bookings[indexPath.row]
+   
       
         
         guard let year = book.booking_date.year, let month = book.booking_date.month, let day = book.booking_date.day else {
             return cell
         }
         cell.Date.text = "\(day)-\(month)-\(year)"
+        cell.testName.text = book.medicalService.name
+        cell.labName.text = book.medicalService.facility.name
+        
+      
         // Configure the cell...
 
         return cell
@@ -53,6 +97,11 @@ class patientBookingTableViewController: UITableViewController {
         if let destination = segue.destination as? viewBookingTableViewController, let selected = tableView.indexPathForSelectedRow{
             destination.selectedTest = bookings[selected.row]
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        categorizeBookings()
+        tableView.reloadData()
     }
   
 
@@ -72,7 +121,7 @@ class patientBookingTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
 
