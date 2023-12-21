c6e38149fd9331f62eb5d4ca331398b9b5adbf6d
nadaalawadhi <nadaalawadhi4@gmail.com>
2023-12-21 17:08:12 +0300

    j

--- PatientHomeFacilityTableViewCell.swift
+++ PatientHomeFacilityTableViewCell.swift
@@ -9,5 +9,14 @@ import UIKit
 
 class PatientHomeFacilityTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var facilityLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+
+    func configure(service: Service){
+        nameLbl.text = service.name
+        facilityLbl.text = service.facility.name
+        priceLbl.text = service.cost
+    }
 
 }
