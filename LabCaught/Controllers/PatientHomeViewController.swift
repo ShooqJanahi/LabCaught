334f0b854e5cdf171d49faf410fe7b89b2c6fd8d
nadaalawadhi <nadaalawadhi4@gmail.com>
2023-12-21 17:23:13 +0300

    patient home view controller

--- LabCaught/Controllers/PatientHomeViewController.swift
+++ LabCaught/Controllers/PatientHomeViewController.swift
@@ -8,16 +8,23 @@
 import UIKit
 
 class PatientHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var services = AppData.services
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return services.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PHFCell", for: indexPath) as! PatientHomeFacilityTableViewCell
+        
+        let service = services[indexPath.row]
+        
+        cell.configure(service: service)
+
+        return cell
     }
     
 
