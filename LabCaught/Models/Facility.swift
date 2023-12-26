83ce2053829301d454b4b5f7603cada1599fe209
ShooqJanahi <121062980+ShooqJanahi@users.noreply.github.com>
2023-12-26 03:23:43 +0300

    added Comments in the Facility.swift file

--- LabCaught/Models/Facility.swift
+++ LabCaught/Models/Facility.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-//enum to specify the type of facility
+// Enum to specify the type of facility (hospital or lab).
 enum FacilityType: String, Codable, Equatable{
     case hospital
     case lab
     
-    //computed property to the enum to provide a string representation fo each
+     // Computed property to provide a string representation for each facility type.
     var description: String{
         switch self{
             case.hospital:
@@ -16,7 +16,9 @@ enum FacilityType: String, Codable, Equatable{
     }
 }
 
+// Facility class inherits from User and adds specific properties and methods that are desigend for a facility.
 class Facility: User{
+    // Properties that are specific to the Facility class.
     var name: String
     var location: String
     var isOpen24Hours: Bool
@@ -24,7 +26,9 @@ class Facility: User{
     var closingTime: DateComponents
     var facilityType: FacilityType
     var logoImageName: String // Name of the image file or URL as a string
-    
+
+   
+    // It initializes both Facility-specific and inherited User properties.
     init(username: String, password: String, phoneNumber: Int, name: String, location: String, isOpen24Hours: Bool, openingTime: DateComponents, closingTime: DateComponents, facilityType: FacilityType, logoImageName: String) {
         self.name = name
         self.location = location
@@ -37,9 +41,10 @@ class Facility: User{
         super.init(username: username, password: password, createdOn: Date(),  phoneNumber: phoneNumber)
     }
     
-
+    // Required initializer for decoding instances of Facility.
     required init(from decoder: Decoder) throws{
         let container = try decoder.container(keyedBy: CodingKeys.self)
+            // Decoding each property using the keys defined in CodingKeys.
             name = try container.decode(String.self, forKey: .name)
             location = try container.decode(String.self, forKey: .location)
             isOpen24Hours = try container.decode(Bool.self, forKey: .isOpen24Hours)
@@ -47,21 +52,26 @@ class Facility: User{
             closingTime = try container.decode(DateComponents.self, forKey: .closingTime)
             facilityType = try container.decode(FacilityType.self, forKey: .facilityType)
             logoImageName = try container.decode(String.self, forKey: .logoImageName)
-            try super.init(from: decoder)
+           // Calling the superclass initializer to decode inherited properties.
+        try super.init(from: decoder)
     }
-    
+
+    // Method to encode instances of Facility.
     override func encode(to encoder: Encoder) throws{
         var container = encoder.container(keyedBy: CodingKeys.self)
-            try container.encode(name, forKey: .name)
+            // Encoding each property using the keys defined in CodingKeys.
+        try container.encode(name, forKey: .name)
             try container.encode(location, forKey: .location)
             try container.encode(isOpen24Hours, forKey: .isOpen24Hours)
             try container.encode(openingTime, forKey: .openingTime)
             try container.encode(closingTime, forKey: .closingTime)
             try container.encode(facilityType, forKey: .facilityType)
             try container.encode(logoImageName, forKey: .logoImageName)
-            try super.encode(to: encoder)
+            // Calling the superclass method to encode inherited properties.
+        try super.encode(to: encoder)
     }
-    
+
+    // Enumeration to define the coding keys used in encoding and decoding.
     private enum CodingKeys: String, CodingKey{
             case name, location, isOpen24Hours, openingTime, closingTime, facilityType, logoImageName
     }
