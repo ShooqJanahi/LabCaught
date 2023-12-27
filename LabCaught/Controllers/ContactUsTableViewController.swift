115c6b1e6a05879f23d222669239b58ba887aabc
Shooq <Shooq.janahi17@hotmail.com>
2023-12-27 12:02:36 +0300

    finished setting up the contact us controller

--- LabCaught/Controllers/ContactUsTableViewController.swift
+++ LabCaught/Controllers/ContactUsTableViewController.swift
@@ -9,77 +9,65 @@ import UIKit
 
 class ContactUsTableViewController: UITableViewController {
 
+    
+    @IBOutlet var ContactUsTxt: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Disabling text editing
+        ContactUsTxt.isEditable = false
+        
+        // Seting up the contact information text
+        setupContactInformationText()
         
     }
 
-    // MARK: - Table view data source
+    // Function to set up the contact information text in the UITextView
+        func setupContactInformationText() {
+            let contactInfoText = """
+            At LabCaught, we understand that forgetting your password can be frustrating. We're here to help you regain access to your account. If you're experiencing any issues with your password, please reach out to our support team for prompt assistance.
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+            Contact Information:
+            Email: support@labcaught.com
+            Phone: +973 17897472
+            Working Hours: 9 AM - 6 PM
+
+            How to Recover Your Password:
+            If you've forgotten your password, follow these steps:
+            Email Support:
+            Send an email to support@labcaught.com with the subject line "Password Recovery Request."
+            Include Required Information:
+            In your email, please include the following information to expedite the process:
+            Your full name
+            Username or CPR
+            Registered email address
+            Any additional details that may help us verify your identity
+            Verification Process:
+            Our support team will review your request and may contact you for additional verification if needed.
+            Password Reset:
+            Once your identity is verified, we will assist you in resetting your password.
+
+            General Inquiries:
+            For general inquiries, feedback, or any other assistance, feel free to contact us using the same email address and phone number provided above.
+            We Are Here to Help:
+
+            LabCaught is committed to providing excellent customer service. Our support team is dedicated to assisting you with any issues you may encounter. Please allow up to 7 working days for a response during our working hours.
+            Thank you for choosing LabCaught for your medical testing needs. We appreciate your trust in our services.
+            """
+            
+            // Seting the contact information text in the UITextView
+            ContactUsTxt.text = contactInfoText
+        }
+
+        override func numberOfSections(in tableView: UITableView) -> Int {
+            // Defining the number of sections in the table view
+            return 1
+        }
+
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // Defining the number of rows in the section
+            return 1
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
