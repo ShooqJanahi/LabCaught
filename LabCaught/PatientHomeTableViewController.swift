56a8c6a3b0dccdbb8fbd624505899ea7904e247e
Shooq <Shooq.janahi17@hotmail.com>
2024-01-04 14:28:23 +0300

    colors

--- LabCaught/PatientHomeTableViewController.swift
+++ LabCaught/PatientHomeTableViewController.swift
@@ -140,13 +140,5 @@ class PatientHomeTableViewController: UITableViewController, UISearchControllerD
         }
         return PatientHomeViewController(coder: coder, facility: choosenFacility)
     }
-    
-    /*
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
