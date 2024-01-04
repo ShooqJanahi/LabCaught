10833ffa60cc2fae7aef17188082ddc975971bbd
Shooq <Shooq.janahi17@hotmail.com>
2024-01-04 17:05:59 +0300

    adding comment

--- LabCaught/BookingDetailsTableViewController.swift
+++ LabCaught/BookingDetailsTableViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class BookingDetailsTableViewController: UITableViewController {
     
+    // Outlets for displaying booking details on the screen.
     @IBOutlet weak var patientNameLbl: UILabel!
     @IBOutlet weak var cprLbl: UILabel!
     @IBOutlet weak var phoneLbl: UILabel!
@@ -19,32 +20,41 @@ class BookingDetailsTableViewController: UITableViewController {
     @IBOutlet weak var instructionsLbl: UILabel!
     @IBOutlet weak var descriptionLbl: UILabel!
     
+    // Outlets for managing booking status and action buttons.
     @IBOutlet weak var buttonCell: UITableViewCell!
     @IBOutlet weak var statusCell: UITableViewCell!
     @IBOutlet weak var statusLbl: UILabel!
     
+    // The current booking is being viewed/edited.
     var booking: booking?
     
+    // initializer with a booking object.
     required init?(coder: NSCoder, booking: booking) {
         self.booking = booking
         super.init(coder: coder)
     }
     
+    // Standard initializer.
     required init?(coder: NSCoder) {
         self.booking = nil
         super.init(coder: coder)
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
-    
+    // Populating the UI with booking details when the view appears.
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         filling()
     }
     
+    // Action for cancelling a booking.
     @IBAction func cancelBooking(_ sender: Any) {
+        // Alert to confirm booking cancellation.
         let alert = UIAlertController(title: "Cancel Booking",
                                           message: "Are you sure you want to cancel this booking?",
                                           preferredStyle: .alert)
@@ -60,7 +70,9 @@ class BookingDetailsTableViewController: UITableViewController {
             present(alert, animated: true, completion: nil)
     }
     
+    // Action for marking a booking as complete.
     @IBAction func completeBooking(_ sender: Any) {
+        // Alert to confirm completion of booking.
         let alert = UIAlertController(title: "Complete Booking",
                                           message: "Are you sure you want to complete this booking?",
                                           preferredStyle: .alert)
@@ -77,7 +89,9 @@ class BookingDetailsTableViewController: UITableViewController {
             present(alert, animated: true, completion: nil)
     }
     
+    // Function to populate UI with booking details
     func filling(){
+        // Update UI based on booking status
         if let bookingStatus = booking?.status {
                 switch bookingStatus {
                 case .upcoming:
@@ -95,6 +109,7 @@ class BookingDetailsTableViewController: UITableViewController {
                 }
             }
         
+        // Populate patient details
         if let firstName = booking?.patient.firstName, let lastName = booking?.patient.lastName {
             patientNameLbl.text = firstName + lastName
         }
@@ -106,7 +121,7 @@ class BookingDetailsTableViewController: UITableViewController {
         if let phone = booking?.patient.phoneNumber {
             phoneLbl.text = String(phone)
         }
-        
+        // Populate booking details
         if let bookingDate = booking?.booking_date {
             bookingDateLbl.text = "\(bookingDate)"
         }
