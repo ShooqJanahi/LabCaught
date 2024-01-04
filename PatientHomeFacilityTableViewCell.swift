08f4f355ffc31972be4c618e1e2d690571fe5d78
Shooq <Shooq.janahi17@hotmail.com>
2024-01-04 16:19:06 +0300

    added comments

--- PatientHomeFacilityTableViewCell.swift
+++ PatientHomeFacilityTableViewCell.swift
@@ -7,15 +7,24 @@
 
 import UIKit
 
+
+// This class represents a cell for displaying information about a patient's home facility service.
 class PatientHomeFacilityTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var nameLbl: UILabel!
-    @IBOutlet weak var facilityLbl: UILabel!
-    @IBOutlet weak var priceLbl: UILabel!
+    
+    // Outlets to the UI elements in the cell.
+    @IBOutlet weak var nameLbl: UILabel!  // Label for displaying the name of the service
+    @IBOutlet weak var facilityLbl: UILabel!    // Label for displaying the name of the facility providing the service
+    @IBOutlet weak var priceLbl: UILabel!   // Label for displaying the price of the service
 
+    
+    // This function configures the cell with data from a `Service` object.
     func configure(service: Service){
+        // Setting the name label to the name of the service.
         nameLbl.text = service.name
+        // Setting the facility label to the name of the facility providing the service.
         facilityLbl.text = service.facility.name
+        // Setting the price label to the cost of the service.
         priceLbl.text = service.cost
     }
 
